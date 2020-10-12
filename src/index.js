df82a3e78ccc4c901ac47908ee41c963977cd1bd
Yuliya Kryvorot <krviua@yahoo.com>
2020-10-12 21:48:48 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketPairs = bracketsConfig.map(bracket => bracket.join(''));
+
+  let previousStr = str;
+
+  do {
+    bracketPairs.forEach((bracketPair, i) => {
+      str = str.replace(bracketPair, '');
+      return str;
+    });
+
+    if (previousStr === str) {
+      return false;
+    }
+
+    previousStr = str;
+  } while (str.length !== 0)
+  
+  return true;
 }
